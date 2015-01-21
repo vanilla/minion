8f161b39fc702ab3f7f8baf1c6cfa21b0141e51d
Tim Gunter <tim@vanillaforums.com>
2015-01-21 16:16:21 -0500

    HotPotato: Colorize output when expiry dwindles

--- HotPotato/js/hotpotato.js
+++ HotPotato/js/hotpotato.js
@@ -77,9 +77,13 @@ function updatePotato() {
     var expiryFormatTime = String(expiryDelay).toFormatTime();
     var potatoName = gdn.definition('PotatoName', 'unknown potato');
     if (expiryDelay >= 1) {
+        var color = '51CEFF';
+        if (expiryDelay < 15) {
+            color = '9F362E';
+        }
         var complianceMessage = '';
         complianceMessage += '<div class="Compliance" style="">';
-        complianceMessage += 'Toss the <b>'+potatoName+'</b> within the next <span style="color:#51CEFF;">'+expiryFormatTime+'</span><br/><br/>';
+        complianceMessage += 'Toss the <b>'+potatoName+'</b> within the next <span style="color:#'+color+';">'+expiryFormatTime+'</span><br/><br/>';
         complianceMessage += '<em>Minion, toss the '+potatoName+' at &lt;mention someone&gt;</em>';
         complianceMessage += '</div>';
     } else {
